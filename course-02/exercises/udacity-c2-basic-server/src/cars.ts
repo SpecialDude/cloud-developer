95fe3768582591b77b541a80cd7de2230bc0de12
SpecialDude <warithadetayo.awa@gmail.com>
2022-09-13 06:54:09 +0100

    Exercise: Get, Filter and Post Cars

--- course-02/exercises/udacity-c2-basic-server/src/cars.ts
+++ course-02/exercises/udacity-c2-basic-server/src/cars.ts
@@ -10,5 +10,7 @@ export const cars: Car[] = [
     { make: 'tesla', type: 'sedan', model: 'roadster', cost: 33, id: 0 },
     { make: 'tesla', type: 'suv', model: 'model 3', cost: 48, id: 1 },
     { make: 'toyota', type: 'sedan', model: 'prius', cost: 22, id: 2 },
-    { make: 'honda', type: 'sedan', model: 'civic', cost: 22, id: 3 }
+    { make: 'honda', type: 'sedan', model: 'civic', cost: 22, id: 3 },
+    { make: 'tesla', type: 'suv', model: 'model 3', cost: 49, id: 4 },
+    { make: 'tesla', type: 'sedan', model: 'model 3', cost: 49, id: 5 },
    ]
--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -8,13 +8,13 @@ import { Car, cars as cars_list } from './cars';
   let cars:Car[]  = cars_list;
 
   //Create an express application
-  const app = express(); 
+  const app = express();
   //default port to listen
-  const port = 8082; 
-  
-  //use middleware so post bodies 
+  const port = 8082;
+
+  //use middleware so post bodies
   //are accessable as req.body.{{variable}}
-  app.use(bodyParser.json()); 
+  app.use(bodyParser.json());
   app.use(express.urlencoded({ extended: true })) //for requests from forms-like data
 
   // Root URI call
@@ -22,10 +22,10 @@ import { Car, cars as cars_list } from './cars';
     res.status(200).send("Welcome to the Cloud!");
   } );
 
-  // Get a greeting to a specific person 
+  // Get a greeting to a specific person
   // to demonstrate routing parameters
   // > try it {{host}}/persons/:the_name
-  app.get( "/persons/:name", 
+  app.get( "/persons/:name",
     ( req: Request, res: Response ) => {
       let { name } = req.params;
 
@@ -54,9 +54,9 @@ import { Car, cars as cars_list } from './cars';
 
   // Post a greeting to a specific person
   // to demonstrate req.body
-  // > try it by posting {"name": "the_name" } as 
+  // > try it by posting {"name": "the_name" } as
   // an application/json body to {{host}}/persons
-  app.post( "/persons", 
+  app.post( "/persons",
     async ( req: Request, res: Response ) => {
 
       const { name } = req.body;
@@ -73,13 +73,77 @@ import { Car, cars as cars_list } from './cars';
   // @TODO Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
 
+  app.get("/cars", (req: Request, res: Response) => {
+    let {make, type, model, cost} = req.query;
+    let cars_list = cars;
+
+    if (make){
+      console.log(make);
+
+      cars_list = cars.filter((car) => car.make == make);
+    }
+
+    if (model){
+      cars_list = cars_list.filter((car) => car.model == model);
+    }
+
+    if (type){
+      cars_list = cars_list.filter((car) => car.type == type);
+    }
+
+    if (cost){
+      cars_list = cars_list.filter((car) => car.cost.toString() === cost);
+    }
+
+    if(cars_list.length === 0){
+      return res.status(404).send(`sorry, car with specified attr(s) is not found`);
+    }
+
+    return res.status(200).send(cars_list);
+  })
+
   // @TODO Add an endpoint to get a specific car
   // it should require id
   // it should fail gracefully if no matching car is found
+  app.get('/cars/:id', (req: Request, res: Response) => {
+    let {id} = req.params;
+    let cars_list: Car[] = cars;
+    cars_list = cars.filter((car) => car.id.toString() == id);
+
+    if (cars_list.length == 0){
+      return res.status(404).send('id not found');
+    }
+
+    return res.status(200).send(cars_list);
+  });
 
   /// @TODO Add an endpoint to post a new car to our list
   // it should require id, type, model, and cost
+app.post('/cars', (req: Request, res: Response) => {
+  let {make, type, model, cost} = req.body;
 
+  if (!(make && type && model && cost)){
+    res.status(404).send(`'make', 'type', 'model' and 'cost' required to post a new car`);
+  }
+
+  let n_cost: number = parseFloat(cost);
+
+  if (isNaN(n_cost)){
+    return res.status(404).send(`cost has to be a number`);
+  }
+
+  let new_car: Car = {
+    make:make,
+    type:type,
+    model:model,
+    cost:n_cost,
+    id:cars.length
+  };
+
+  cars.push(new_car);
+
+  return res.status(200).send(new_car);
+});
   // Start the Server
   app.listen( port, () => {
       console.log( `server running http://localhost:${ port }` );
